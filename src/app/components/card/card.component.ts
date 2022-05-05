0c84f78934dbeed7dda005c17b760fdf4c103533
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-05 12:37:18 -0500

    Se agrega modal para reproducir los videos.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,16 +1,34 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Video } from "src/app/models/youtube.models";
 
+import Swal from "sweetalert2";
+
 @Component({
   selector: "app-card",
   templateUrl: "./card.component.html",
   styleUrls: ["./card.component.css"],
 })
 export class CardComponent implements OnInit {
-  @Input() public video!: Video;
+  @Input()
+  public video!: Video;
 
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  public playVideo(video: Video): void {
+    Swal.fire({
+      html: `
+      <h4>${video.title}</h4>
+      <hr>
+      <iframe width="100%" height="315" 
+        src="https://www.youtube.com/embed/${video.resourceId.videoId}"}" 
+        title="YouTube video player" 
+        frameborder="0" allow="accelerometer; autoplay; 
+        clipboard-write; encrypted-media; gyroscope; 
+        picture-in-picture" allowfullscreen>
+      </iframe>`,
+    });
   }
 }
