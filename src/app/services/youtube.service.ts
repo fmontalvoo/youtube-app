c5087d59c032e5326df4ca795a37a3fb7269e02d
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-05 10:12:20 -0500

    Se crea modelo para dar formato a la respuesta del API.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -2,19 +2,26 @@ import { Injectable } from "@angular/core";
 
 import { HttpClient, HttpParams } from "@angular/common/http";
 
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs/internal/Observable";
+
+import { Item, Video, YoutubeModel } from "../models/youtube.models";
+
 import { environment } from "src/environments/environment.dev";
 
 @Injectable({
   providedIn: "root",
 })
 export class YoutubeService {
-  private API_KEY = environment.YoutubeApiKey;
-  private PLAYLIST_ID = environment.playlistId;
-  private API_URL = "https://www.googleapis.com/youtube/v3";
+  private API_KEY: string = environment.YoutubeApiKey;
+  private PLAYLIST_ID: string = environment.playlistId;
+  private API_URL: string = "https://www.googleapis.com/youtube/v3";
+
+  private nextPageToken: string = "";
 
   constructor(private http: HttpClient) {}
 
-  public getVideos() {
+  public getVideos(): Observable<Video[]> {
     const url = `${this.API_URL}/playlistItems`;
     const params = new HttpParams()
       .set("part", "snippet")
@@ -22,6 +29,13 @@ export class YoutubeService {
       .set("key", this.API_KEY)
       .set("playlistId", this.PLAYLIST_ID);
 
-    return this.http.get(url, { params });
+    return this.http.get<YoutubeModel>(url, { params })
+      .pipe(
+        map((resp: YoutubeModel) => {
+          this.nextPageToken = resp.nextPageToken;
+          return resp.items;
+        }),
+        map((items: Item[]) => items.map((item) => item.snippet)),
+      );
   }
 }
